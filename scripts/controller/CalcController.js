57f33e177b3d3b07cbc58c890c977b0d33e70788
Alexander <alexander.nakahara@ludospro.com.br>
2020-01-03 01:09:30 -0300

    Code refactoring

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,34 +2,47 @@ class CalcController {
 
     constructor() {
 
-        this._displayCalc = "0";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+
         this._currentDate;
         this.initialize();
     }
 
-    initialize(){
-       let displayCalcEl = document.querySelector("#display");
-       let dateEl = document.querySelector("#data");
-       let timeEl = document.querySelector("#hora");
+    initialize() {
 
-       displayCalcEl.innerHTML = "666";
-       dateEl.innerHTML = "10/06/1999";
-       timeEl.innerHTML = "10:20";
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value) {
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual() {
+    get currentDate() {
         return this._currentDate;
     }
 
-    set dataAtual(value) {
+    set currentDate(value) {
         this._currentDate = value;
     }
 
