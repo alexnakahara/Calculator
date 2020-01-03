d4d880f82616a731b403d5ba024b2b7a24171a4a
Alexander <alexander.nakahara@ludospro.com.br>
2020-01-03 04:53:13 -0300

    Identifying of buttons
    In the commit was used addEventListener, splite,  querySelectorAll, classname.
    "baseval" was usaded, because the class is a svg.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,7 +4,6 @@ class CalcController {
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
     this._timeEl = document.querySelector("#hora");
-
     this._currentDate;
     this.initialize();
     this.initButtonsEvents();
@@ -59,15 +58,27 @@ class CalcController {
     this._currentDate = value;
   }
 
+  addEventListenerAll(element, events, fn) {
+    events.split(' ').forEach(event => {
+
+      element.addEventListener(event, fn, false);
+
+    });
+  }
+
   initButtonsEvents() {
     let buttons = document.querySelectorAll("#buttons > g, #parts > g");
     buttons.forEach((btn, index) => {
 
-      btn.addEventListener('click', e => {
+      this.addEventListenerAll(btn, "click drag", e => {
 
         console.log(btn.className.baseVal.replace("btn-", ""));
-        
-      })
+
+      });
+
+      this.addEventListenerAll(btn, "mouseover", e => {
+        btn.style.cursor = "pointer";
+      });
 
     });
   }
