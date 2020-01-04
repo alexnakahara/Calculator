2cf4b9bdec6df982dcdaf75389c0e7ee9363fc7f
Alexander <alexander.nakahara@ludospro.com.br>
2020-01-04 03:19:53 -0300

    Calculation and validation of numbers and operators

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,9 +1,10 @@
 class CalcController {
   constructor() {
-    this._locale = "pt-BR";
+    this._operation = [];
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
     this._timeEl = document.querySelector("#hora");
+    this._locale = "pt-BR";
     this._currentDate;
     this.initialize();
     this.initButtonsEvents();
@@ -58,6 +59,154 @@ class CalcController {
     this._currentDate = value;
   }
 
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value;
+  }
+
+  isOperator(value) {
+
+    return (['+', '-', '%', '/', '*'].indexOf(value) > -1);
+  }
+
+  pushOperation(value) {
+    this._operation.push(value);
+
+    if (this._operation.length > 3) {
+
+      this.calc();
+
+    }
+  }
+  
+  calc() {
+    let last = this._operation.pop();
+
+    let result = eval(this._operation.join(""));
+
+    this._operation = [result, last];
+    this.setLastNumberDisplay();
+
+  }
+
+  setLastNumberDisplay() {
+
+    let lastNumber;
+    for (let i = this._operation.length - 1; i >= 0; i--) {
+
+      if (!this.isOperator(this._operation[i])) {
+          lastNumber = this._operation[i];
+          break;
+      }
+
+    }
+
+    this.displayCalc = lastNumber;
+
+  }
+
+  addOperation(value) {
+
+    if (isNaN(this.getLastOperation())) {
+      //string
+      if (this.isOperator(value)) {
+
+        this.setLastOperation(value);
+
+      } else if (isNaN(value)) {
+        console.log("outra coisa ===", value);
+
+      } else {
+        //primeira vez que adicionou um número
+        this.pushOperation(value);
+        this.setLastNumberDisplay();
+        console.log("primeira vez", value);
+
+      }
+    } else {
+      //number
+      if (this.isOperator(value)) {
+
+        this.pushOperation(value);
+
+      } else {
+
+        let newValue = this.getLastOperation().toString() + value;
+        this.setLastOperation(parseInt(newValue));
+
+        //update display
+        this.setLastNumberDisplay();
+      }
+
+    }
+    console.log(this._operation)
+  }
+
+  clearAll() {
+    this._operation = [];
+  }
+
+  clearEntry() {
+    this._operation.pop();
+  }
+
+  setError() {
+    this.displayCalc = "Error"
+  }
+
+  execBtn(value) {
+    switch (value) {
+      case 'ac':
+        this.clearAll();
+        break;
+      case 'ce':
+        this.clearEntry();
+        break;
+      case 'soma':
+        this.addOperation('+');
+        break;
+      case 'subtracao':
+        this.addOperation('-');
+        break;
+      case 'divisao':
+        this.addOperation('/');
+        break;
+      case 'multiplicacao':
+        this.addOperation('*');
+        break;
+      case 'porcento':
+        this.addOperation('%');
+        break;
+      case 'igual':
+
+        break;
+      case 'ponto':
+        this.addOperation('.');
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.addOperation(parseInt(value));
+        break;
+
+      default:
+        this.setError();
+        break;
+
+    }
+  }
+
   addEventListenerAll(element, events, fn) {
     events.split(' ').forEach(event => {
 
@@ -72,7 +221,9 @@ class CalcController {
 
       this.addEventListenerAll(btn, "click drag", e => {
 
-        console.log(btn.className.baseVal.replace("btn-", ""));
+        // console.log(btn.className.baseVal.replace("btn-", ""));
+        let textBtn = btn.className.baseVal.replace("btn-", "");
+        this.execBtn(textBtn);
 
       });
 
